e92fd6b4be6e7170e6f73e39f53a1668037b34e7
Lucas Manuel Alface <112022654+Desenvolvedormoz@users.noreply.github.com>
2022-10-11 03:16:01 -0300

    Já instalei as bibliotecas de navegação
    Acabei de instalar as bibliotecas de navegação, testei e tudo está certo. Agora vou começar a adicionar os conteúdos de cada componente e personalizar as abas de navegação e a aba cabeçalho.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TelaInflacao from './componentes/TelaInflacao';
 import TelaSalario from './componentes/TelaSalario';
@@ -19,11 +19,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
